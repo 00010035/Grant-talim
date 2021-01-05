9e92390db08cf3c828663cb9a91ddf7042e61e77
00010035 <ifatkhullaev2@students.wiut.uz>
2021-01-05 17:38:44 +0500

    first step
    qwd

---
+++ app.js
@@ -0,0 +1,9 @@
+function sumNumbers (a, b){}
+
+let str = 'Some string';
+let arr = ['John', 'Mike', 'Ibroxim'];
+
+console.log(str.length);
+console.log(arr.length);
+
+console,log{sumNumbers.length};
